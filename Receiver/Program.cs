d6501272c2bbc2535a5e07ec3be7ed1c51977610
ankit choubey <ankitchoubey800@gmail.com>
2021-10-07 20:54:22 +0530

    Sender And Receiver Code

---
+++ Receiver/Program.cs
@@ -0,0 +1,33 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Receiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "Queue_Test", durable: false, exclusive: false, autoDelete: false, arguments: null);
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    byte[] body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine("Received Message: {0}", message);
+                };
+                channel.BasicConsume("Queue_Test", autoAck: true, consumer: consumer);
+            }
+            Console.WriteLine("Press [enter] to exit");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Sender/Program.cs
@@ -0,0 +1,28 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using(var connection = factory.CreateConnection())
+                using(var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "Queue_Test", durable: false, exclusive: false,autoDelete:false,arguments:null);
+                string message = "I came from Queue.";
+                var body = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish(exchange: "", routingKey: "Queue_Test", basicProperties: null, body: body);
+                Console.WriteLine("Sent Message: {0}",message);
+            }
+            Console.WriteLine("Press [enter] to exit");
+            Console.ReadLine();
+        }
+    }
+}
